2459bd3092cb928df40f531ee1645a5b35dc5bfb
Thomas Madden <tmadden@mgh.harvard.edu>
2018-01-24 17:00:35 -0500

    Add Python dev package.

--- scripts/set-up-system.sh
+++ scripts/set-up-system.sh
@@ -3,7 +3,7 @@ echo "Setting up system..."
 add-apt-repository -y ppa:ubuntu-toolchain-r/test
 add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update -qy
-apt-get install -y --upgrade python3.5 g++-5 gcc-5 lcov cmake git curl
+apt-get install -y --upgrade python3.5 python3.5-dev g++-5 gcc-5 lcov cmake git curl
 wget https://bootstrap.pypa.io/get-pip.py -O /tmp/get-pip.py
 python3.5 /tmp/get-pip.py
 python3.5 -m pip install virtualenv
