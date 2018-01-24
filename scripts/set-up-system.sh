83499032474013c8290939c9aa2dfe791e3b7406
Thomas Madden <tmadden@mgh.harvard.edu>
2018-01-24 16:52:21 -0500

    Fix pip installation.

--- scripts/set-up-system.sh
+++ scripts/set-up-system.sh
@@ -3,5 +3,8 @@ echo "Setting up system..."
 add-apt-repository -y ppa:ubuntu-toolchain-r/test
 add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update -qy
-apt-get install -y --upgrade python3.5 python3.5-pip g++-5 gcc-5 lcov cmake git curl
-pip3.5 install virtualenv
+apt-get install -y --upgrade python3.5 g++-5 gcc-5 lcov cmake git curl
+wget https://bootstrap.pypa.io/get-pip.py -O /tmp/get-pip.py
+python3.5 /tmp/get-pip.py
+python3.5 -m pip install virtualenv
+rm /tmp/get-pip.py
