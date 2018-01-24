8d229b9ab88ffac35ccf0e6ba6a68f6cb641a136
Thomas Madden <tmadden@mgh.harvard.edu>
2018-01-24 15:51:56 -0500

    Move apt depedencies to setup.sh.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-python --version
-if [ `python -c 'import sys; print("%i" % (sys.hexversion>=0x03050000))'` -eq 0 ]; then
-    echo "At least Python 3.5 is required."
-    exit 1
-fi
 echo "Setting up build prerequisites..."
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update -qy
+sudo apt-get install -y --upgrade python3 python3-pip g++-5 gcc-5 lcov cmake git curl
+sudo pip3 install virtualenv
+virtualenv --python=python3 .python
+source .python/bin/activate
+python --version
 pip install conan jinja2 gcovr pytest websocket-client
 conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
 conan remote add conan-community https://api.bintray.com/conan/conan-community/conan
