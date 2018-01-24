ba0da602c07790ec9d8044c869cdfe6f2defa94a
Thomas Madden <tmadden@mgh.harvard.edu>
2018-01-24 16:45:10 -0500

    Force Python 3.5 in Ubuntu Trusty.

--- scripts/set-up-system.sh
+++ scripts/set-up-system.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Setting up system..."
 add-apt-repository -y ppa:ubuntu-toolchain-r/test
+add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update -qy
-apt-get install -y --upgrade python3 python3-pip g++-5 gcc-5 lcov cmake git curl
-pip3 install virtualenv
+apt-get install -y --upgrade python3.5 python3.5-pip g++-5 gcc-5 lcov cmake git curl
+pip3.5 install virtualenv
