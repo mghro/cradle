e0329e7ce27e9e3d5c7d266478e232885144b6f1
Thomas Madden <tmadden@mgh.harvard.edu>
2018-01-29 17:05:13 -0500

    Use curl to get pip.

--- scripts/set-up-system.sh
+++ scripts/set-up-system.sh
@@ -7,7 +7,5 @@ add-apt-repository -y ppa:ubuntu-toolchain-r/test
 add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update -qy
 apt-get install -y --upgrade python3.5 python3.5-dev g++-5 gcc-5 lcov cmake git curl
-wget https://bootstrap.pypa.io/get-pip.py -O /tmp/get-pip.py
-python3.5 /tmp/get-pip.py
+curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | python3.5
 python3.5 -m pip install virtualenv
-rm /tmp/get-pip.py
