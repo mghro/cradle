f65d1cb2c5249fadb2915e1936bcbea38b65be05
Thomas Madden <tmadden@mgh.harvard.edu>
2019-03-05 11:30:23 -0500

    Fix Travis Python environment.

--- scripts/set-up-python.sh
+++ scripts/set-up-python.sh
@@ -2,7 +2,7 @@
 # Set up a Python virtual environment so that it can build CRADLE.
 echo "Setting up Python environment in .venv..."
 set -x -e
-virtualenv --python=python3 --prompt="(cradle) " .venv
+virtualenv "$@" --prompt="(cradle) " .venv
 source .venv/bin/activate
 python --version
 pip install conan jinja2 gcovr pytest websocket-client msgpack
